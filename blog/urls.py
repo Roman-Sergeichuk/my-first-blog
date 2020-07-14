b73864065a47fd0d85c22d19919c32c6ce7a20a7
Roman-Sergeichuk <sergeichuk.roman@mail.ru>
2020-07-14 08:29:23 +0300

    user aurhentication was added

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/edit/<int:pk>', views.post_edit, name='post_edit'),
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
+    path('post/<int:pk>/post_publish', views.post_publish, name='post_publish'),
+    path('post/<int:pk>/post_remove', views.post_remove, name='post_remove'),
 ]
